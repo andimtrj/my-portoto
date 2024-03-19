5d3987d39cdf535a37af2010a6c71e1567547aa5
Andi Mataraja <amataraja@gmail.com>
2024-03-19 22:24:27 +0700

    homepage created

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,25 +1,95 @@
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import "../style/home.css";
+import Image from "react-bootstrap/Image";
+import ImageAndi from "../assets/img-andi.png";
+import ImageHere from "../assets/img-here.png";
 
 function Home(params) {
   return (
     <>
       <div className="big-box">
-        <Row>
-          <Col>
-            <h1 className="title">Hi There</h1>
-          </Col>
-          <Col xs={6}>
-            <h3 className="desc-title">Ini Title Desc</h3>
-            <p className="desc">Ini Description</p>
-          </Col>
-          <Col>3 of 3</Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col xs={5}>2 of 3 (wider)</Col>
-          <Col>3 of 3</Col>
-        </Row>
+        <div className="box">
+          <div className="title-container col">
+            <div className="line"></div>
+            <h1 className="title">Hello There</h1>
+          </div>
+          <div className="col">
+            <Image src={ImageAndi} alt="big-image" className="img-andi" />
+          </div>
+          <div className="title-container col">
+            <h1 className="title">I'm Andi</h1>
+            <div className="line line-2"></div>
+          </div>
+        </div>
+
+        <div className="box middle">
+          <div className="col middle">
+            <div className="about-content">
+              <h3 className="desc-title">[HELLO]</h3>
+              <p className="desc">
+                Visitors! Welcome to my little corner of the web! Call me Andi
+                and thanks for visiting my website. Sit back, fasten your
+                seatbelt, and enjoy your exploration on my website.
+              </p>
+            </div>
+            <div className="about-content">
+              <h3 className="desc-title">[MAY I INTRODUCE MY SELF?]</h3>
+              <p className="desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Aliquam faucibus purus in massa. Urna id volutpat lacus laoreet
+                non curabitur gravida arcu ac. Eu scelerisque felis imperdiet
+                proin fermentum leo vel. Aliquam sem fringilla ut morbi
+                tincidunt augue. Etiam non quam lacus suspendisse faucibus
+                interdum posuere. Sit amet nulla facilisi morbi tempus iaculis
+                urna.
+              </p>
+            </div>
+            <div className="about-content">
+              <h3 className="desc-title">[WHAT CAN I DO FOR YOU]</h3>
+              <p className="desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Maecenas accumsan lacus vel facilisis volutpat est velit egestas
+                dui. Mauris rhoncus aenean vel elit. Tempus urna et pharetra
+                pharetra massa massa.
+              </p>
+            </div>
+          </div>
+
+          <div className="col middle">
+            <div className="skills">
+              <div className="desc-title">[SKILLS]</div>
+              <div className="desc-title">
+                <ul>
+                  <li>FIGMA</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JAVASCRIPT</li>
+                  <li>LARAVEL</li>
+                  <li>REACT JS</li>
+                  <li>BOOTSTRAP</li>
+                  <li>TAILWIND</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </div>
+
+            <div here-container>
+              <div className="img-here">
+                <Image src={ImageHere} />
+              </div>
+            </div>
+
+            <div className="go-button">
+              <button className="desc-title">GO SEE MY WORKS</button>
+            </div>
+
+            <div className="skills year-born desc-title">
+              <div className="gapping">2023</div>
+              <div className="gapping rock-left">STILL ROCKIN'</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
